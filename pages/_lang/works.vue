f510dd38434ddd371dffd403f70e833c442f4c1e
taka66 <taka66.iphone@gmail.com>
2019-06-13 07:55:06 -0700

    Fix align issue

--- pages/_lang/works.vue
+++ pages/_lang/works.vue
@@ -7,7 +7,11 @@
           <a href="https://takahirofujii.com">
             <img class="thumbnail" src="~/assets/test.jpg" />
           </a>
-          <p>test</p>
+          <p class="thumbnail-p">2019</p>
+          <p class="thumbnail-p">React Tokyo Plus</p>
+          <p class="thumbnail-p">
+            Large react product with 20+ FrontEnd Engineers in Rakuten Travel
+          </p>
         </div>
         <div class="card">
           <a href="https://takahirofujii.com">
@@ -38,18 +42,24 @@ export default {}
 
 <style lang="scss">
 .flex {
-  width: 80%;
+  width: 90%;
   display: flex;
   align-items: flex-start;
   flex-wrap: wrap;
   margin: auto;
 }
 .card {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
   padding: 10px;
   width: 50%;
 }
 .thumbnail {
   width: 100%;
+}
+.thumbnail-p {
+  font-size: 20px;
 }
 a:hover .thumbnail {
   opacity: 0.7;
