34bffe9990064f0a918e9930b192ab318a1c3cbc
taka66 <taka66.iphone@gmail.com>
2019-06-14 11:17:59 -0700

    Update works contents

--- pages/_lang/works.vue
+++ pages/_lang/works.vue
@@ -6,30 +6,71 @@
         <div class="card">
           <a href="https://takahirofujii.com">
             <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2019</p>
+            <p class="thumbnail-p">React Tokyo Plus</p>
+            <p class="thumbnail-p">
+              Large react product with 20+ FrontEnd Engineers in Rakuten Travel
+            </p>
           </a>
-          <p class="thumbnail-p">2019</p>
-          <p class="thumbnail-p">React Tokyo Plus</p>
-          <p class="thumbnail-p">
-            Large react product with 20+ FrontEnd Engineers in Rakuten Travel
-          </p>
         </div>
         <div class="card">
           <a href="https://takahirofujii.com">
             <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2019</p>
+            <p class="thumbnail-p">React Meetup</p>
+            <p class="thumbnail-p">楽天トラベルのフロントエンドについて</p>
           </a>
-          <p>test</p>
         </div>
         <div class="card">
           <a href="https://takahirofujii.com">
             <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2018</p>
+            <p class="thumbnail-p">Microservice Meetup Vol.6</p>
+            <p class="thumbnail-p">
+              FrontEndからみるmicroserviceとBackendからみるmicroservice
+            </p>
           </a>
-          <p>test</p>
         </div>
         <div class="card">
           <a href="https://takahirofujii.com">
             <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2017</p>
+            <p class="thumbnail-p">東京理科大学 - 公演</p>
+            <p class="thumbnail-p">
+              グローバルな環境におけるエンジニアのキャリア形成
+            </p>
           </a>
-          <p>test</p>
+        </div>
+        <div class="card">
+          <a href="https://takahirofujii.com">
+            <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2017</p>
+            <p class="thumbnail-p">東京理科大学 - 寄稿(科学フォーラム)</p>
+            <p class="thumbnail-p">
+              好きなことを仕事にする
+            </p>
+          </a>
+        </div>
+        <div class="card">
+          <a href="https://takahirofujii.com">
+            <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2016</p>
+            <p class="thumbnail-p">Spring Day 2016</p>
+            <p class="thumbnail-p">
+              楽天トラベルとSpring
+            </p>
+          </a>
+        </div>
+        <div class="card">
+          <a href="https://takahirofujii.com">
+            <img class="thumbnail" src="~/assets/test.jpg" />
+            <p class="thumbnail-p">2015</p>
+            <p class="thumbnail-p">JSUG(Japan Spring User Group)</p>
+            <p class="thumbnail-p">
+              Spring Rest
+              Docs(SpringOneを経験してよりよいWebサービスを作るために僕らが取り組むこと)
+            </p>
+          </a>
         </div>
       </div>
     </div>
@@ -55,11 +96,19 @@ export default {}
   padding: 10px;
   width: 50%;
 }
+.card a {
+  text-decoration: none;
+  color: #373c38;
+}
+.card a:hover {
+  text-decoration: none;
+  color: #38a1db; //Tsuyukusa
+}
 .thumbnail {
   width: 100%;
 }
 .thumbnail-p {
-  font-size: 20px;
+  font-size: 14px;
 }
 a:hover .thumbnail {
   opacity: 0.7;
