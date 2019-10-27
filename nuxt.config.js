9e3bcf4588575f6ea48dc57cb8577fabb30dc2a7
taka66 <taka66.iphone@gmail.com>
2019-10-27 19:19:15 +0900

    Support OGP meta tag.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,34 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'UI/UX Engineer',
+    titleTemplate: "%s | Takahiro Fujii's portfolio",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'takahirofujii.dev'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://takahirofujii.dev'
+      },
+      { hid: 'og:title', property: 'og:title', content: 'takahirofujii.dev' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: "UI/UX engineer Takahiro Fujii's portfolio website "
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://takahirofujii.dev/ogp/common.png'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
